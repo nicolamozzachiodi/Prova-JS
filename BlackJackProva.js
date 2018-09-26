c6d3489d271fbc0f9133fcfb3021766b00e504bf
nicolamozzachiodi <43537327+nicolamozzachiodi@users.noreply.github.com>
2018-09-26 22:33:41 +0200

    Add files via upload

--- BlackJackProva.js
+++ BlackJackProva.js
@@ -13,13 +13,13 @@ var player = c + d;
 console.log("Il banchiere ha totalizzato " + Math.round(banchiere));
 console.log("Il giocatore ha totalizzato " + Math.round(player));
 
-if (banchiere >= player && banchiere <=15){
+if (banchiere >= player && banchiere <=15) {
     console.log("Banchiere Vince");
     }
-    else if (banchiere < player && player <=15)
-    {
-        console.log("Player Vince");
-    }
-    else {
-        console.log("Non vince nessuno");
-    }
+        else if (banchiere < player && player <=15)
+        {
+            console.log("Player Vince");
+            }
+            else {
+                console.log("Non vince nessuno");
+            }
